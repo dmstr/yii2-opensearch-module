feee447b4c6a73eb441140b4f071e02602706845
Kanabanarama <kanabanarama@googlemail.com>
2017-09-25 14:05:36 +0200

    Cleanup

--- src/widgets/Opensearch.php
+++ src/widgets/Opensearch.php
@@ -43,21 +43,8 @@ class Opensearch extends Widget
         $this->ossApi = new Handler(array('url' => $this->apiUrl, 'key' => $this->apiKey, 'login' => $this->apiLogin));
     }
 
-    /*
-     * In case the oss client lib works with PHP 7.1, it would be used like this
-     */
     public function run() {
-        // LIST FIELDS
-        /*$request = new \OpenSearchServer\Field\GetList();
-        $request->index('hrzg');
-        $response = $this->ossApi->submit($request);
-        foreach($response as $key => $item) {
-            echo '<br/>Item #'.$key .': ';
-            print_r($item);
-        }*/
-
         $query = '';
-        $page = 1;
         $renderSearchbox = true;
         $postData = \Yii::$app->request->post();
         $getData = \Yii::$app->request->get();
@@ -72,19 +59,10 @@ class Opensearch extends Widget
         } else if(isset($getData['query'])) {
             $query = $getData['query'];
         }
-        $autocomplete = [];
-        $results = [];
         $page = (isset($getData['page'])) ? intval($getData['page']) : 1;
 
+        $results = [];
         if($query) {
-            // autocomplete
-            $autocompleteRequest = new \OpenSearchServer\Autocompletion\Query();
-            $autocompleteRequest->index($this->index)
-                ->name('autocomplete')
-                ->query($query)
-                ->rows($this->resultsPerPage);
-            $autocomplete = $this->ossApi->submit($autocompleteRequest);
-            // default search for testing
             $offset = ($page-1)*$this->resultsPerPage;
             $request = new Search();
             $request->index($this->index)
@@ -95,22 +73,14 @@ class Opensearch extends Widget
             $results = $this->ossApi->submit($request);
         }
 
-        // DYNAMIC MODEL
-        /*$model = new \yii\base\DynamicModel(['searchword']);
-        if($model->load(\Yii::$app->request->post())){
-            var_dump($model->getSearchword());
-        }*/
-
         $pages = ($results) ? ceil($results->getTotalNumberFound() / $this->resultsPerPage) : 1;
 
         return \Yii::$app->view->render('@hrzg/opensearch/views/opensearch.twig', [
-            'autocomplete' => $autocomplete,
             'query' => $query,
             'renderSearchbox' => $renderSearchbox,
             'results' => $results,
             'pages' => $pages,
             'page' => $page
-            //'model' => $model
         ]);
     }
 }
